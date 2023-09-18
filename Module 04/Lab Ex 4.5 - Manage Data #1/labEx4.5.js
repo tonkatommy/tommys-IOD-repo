7b1b1bf5b670b2c180bf77d910f9232d4f20b993
Tommy Goodman <tommysdevelopment@gmail.com>
2023-09-19 02:16:06 +1200

    Completed: Lab Ex 4.5 - Manage Data #1

--- Module 04/Lab Ex 4.5 - Manage Data #1/labEx4.5.js
+++ Module 04/Lab Ex 4.5 - Manage Data #1/labEx4.5.js
@@ -1,30 +1,75 @@
-const cards = [
-  {
-    title: "Audi",
-    description:
-      "Audi AG is a German automotive manufacturer of luxury vehicles headquartered in Ingolstadt, Bavaria, Germany.",
-  },
-  {
-    title: "Mercedes-Benz",
-    description:
-      "Mercedes-Benz, commonly referred to as Mercedes, is a German luxury automotive brand.",
-  },
-  {
-    title: "BMW",
-    description:
-      "Bayerische Motoren Werke AG, commonly referred to as BMW, is a German multinational corporate manufacturer of luxury vehicles and motorcycles headquartered in Munich, Bavaria, Germany.",
-  },
-];
-function addCard(card) {
+import * as news from "./news.js";
+
+const newsItems = news.news;
+const maxNews = 6;
+
+function addNewsItem(newsItem) {
   const template = document
     .getElementById("card-template")
     .content.cloneNode(true);
-  template.querySelector(".card-title").innerText = card.title;
-  template.querySelector(".card-text").innerText = card.description;
-  document.querySelector("#car-list").appendChild(template);
+  template.querySelector(".card-title").innerText = newsItem.title;
+  template.querySelector(".card-text").innerText = newsItem.content;
+  document.querySelector("#my-news-items").appendChild(template);
 }
-if ("content" in document.createElement("template")) {
-  cards.forEach((card) => {
-    addCard(card);
+
+const loadNews = (newsItems) => {
+  let element = document.querySelector("#my-news-items");
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  newsItems.forEach((newsItem) => {
+    addNewsItem(newsItem);
   });
-}
+};
+
+const updateNews = () => {
+  return new Promise(() => {
+    setInterval(() => {
+      if (newsItems.length > maxNews) {
+        newsItems.pop(newsItems.length);
+      }
+      loadNews(newsItems);
+    }, 5000);
+  });
+};
+
+loadNews(newsItems);
+updateNews();
+
+// event listener for the save news button
+document.querySelector("#btn-save-news").addEventListener("click", () => {
+  // save input to variables and clear input field
+  const newsTitle = document.querySelector("#titleInput").value;
+  document.querySelector("#titleInput").value = "";
+  const newsText = document.querySelector("#newsInput").value;
+  document.querySelector("#newsInput").value = "";
+  // create a new news item
+  const newItem = {
+    id: nextId(newsItems),
+    title: newsTitle,
+    content: newsText,
+  };
+  // add new item to start of array
+  newsItems.unshift(newItem);
+  // check that news array doesn't exceed max items
+  while (newsItems.length > maxNews) {
+    newsItems.pop();
+  }
+});
+
+// function to get the next ID number
+const nextId = (newsItems) => {
+  const sortedArr = newsItems.slice().sort((a, b) => {
+    a.id - b.id;
+  });
+  let lastId = 0;
+  sortedArr.forEach((newsItem) => {
+    if (newsItem.id != lastId + 1) {
+      // found a gap, use this id
+      return lastId + 1;
+    }
+    lastId = newsItem.id;
+  });
+  // no gaps found, use lat id + 1
+  return lastId + 1;
+};
